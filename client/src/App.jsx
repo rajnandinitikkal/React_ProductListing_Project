7d35ed79830bd987c8408b11d767abb7e9a4ecb3
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-07-31 22:47:26 +0530

    added two pages that shows output

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@
 import Navbar from "./component/Navbar/Navbar";
 import Login from "./view/login.jsx";
 import SignUp from "./view/Signup.jsx";
-import Products from "./view/Productcard.jsx";
+// import Products from "./view/Productcard.jsx";
 import ProductDetailPage from "./view/ProductDetailPage.jsx";
-// import Products from "./component/ProductPage.jsx";
+import Products from "./component/Productcard.jsx";
 import About from "./view/About/About";
 import Feedback from "./view/FeedbackPage.jsx";
 import Footer from "./component/footer.jsx";
--- client/src/view/ProductDetailPage.jsx
+++ client/src/view/ProductDetailPage.jsx
@@ -1,35 +1,31 @@
 import React from "react";
 import productsData from "../data/products.json";
-// import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 
 function ProductDetailPage(props) {
   const { productId } = useParams();
-  const products = productsData.find((p) => p.id === parseInt(productId));
+  const product = productsData.find((p) => p.id === parseInt(productId));
 
-  //  const [products, setProducts] = useState([]);
-  //     useEffect(() => {
-  //       setProducts(productsData);
-  //     }, []);
+  if(!product){
+    return <p>Product not found.</p>
+  }
+
   return (
-    <>
-      {
-        ////////// This is for fetching JSON data ///////
-        products.map((product) => (
-          <div key={product.id} className="productCard">
-            <div className="ImageURL">
-              <img src={product.imgUrl} alt={product.name}></img>
-            </div>
-            <div className="product-info">
-              <p> {product.ncdc}</p>
-              <p> {product.description}</p>
-              <p> {product.code}</p>
-              <p> {product.honda}</p>
-            </div>
-          </div>
-        ))
-      }
-    </>
+    <div className="productDetailPage">
+      <div className="productDetailPageImage">
+        <img src={product.imgUrl} alt={product.name} />
+      </div>
+      <div className="productDetailPageInformation">
+        <p>{product.name}</p>
+        <p>{product.description}</p>
+      <div>
+        <p>{product.price}</p>
+        <p>{product.category}</p>
+        <p>{product.Rating}</p>
+      </div>  
+        <button>Add To Bag</button>
+      </div>
+    </div>
   );
 }
 
