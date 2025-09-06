189bf65ff64fdb26e5ee35676df8a45f8c8e91a6
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-09-06 21:58:19 +0530

    in landing page featured categories are add

---
+++ client/src/component/Categoriesgrid.jsx
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import { useEffect, useState } from 'react';
+// import "../Styles/LandingPage.css";
+// import CategoriesData from "../data/Categories.json"
+
+
+// function Categoriesgrid() {
+//     const [categories, setCategories] = useState([]);
+    
+//     useEffect(() =>{
+//     setCategories(CategoriesData); },
+// []
+// )
+
+//   return (
+//     <>
+//     {
+//         <div className="categoriesshopgrid">
+//           <div className="ring">  <img src={categories.imgUrl} alt={categories.name} className="categoryImage"></img> </div>
+//             <p>{categories.name}</p>
+//         </div>
+//     }
+//     </>
+//   )
+// }
+
+// export default Categoriesgrid
--- client/src/view/LandingPage.jsx
+++ client/src/view/LandingPage.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+// import React from 'react';
 import homeImg from "../assets/LandingPagee.png";
+import "../Styles/LandingPage.css";
+// import CategoriesData from "../data/Categories.json";
 
-function LandingPage() {
+function LandingPage(props) {
+  // const categories = CategoriesData.find();
   return (
-    <>LandingPage
-    <img src={homeImg} alt="landing-page"></img>
-    
+    <>
+    <div>
+    <img src={homeImg} alt="landing-page" className='Clothes_girlImage'></img>
+    </div>
+
+    <div>
+      {/* <h2>Categories</h2>
+      <div>
+        <img src={categories.imgUrl} alt={categories.name}></img>
+        <p>{categories.name}</p>
+      </div> */}
+
+
+    </div>
+
+    <div>
+
+    </div>
+
     </>
   )
 }
