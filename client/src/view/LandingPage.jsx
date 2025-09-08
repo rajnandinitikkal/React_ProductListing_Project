c487e42897984c800d26fba0d622f2ce7d8d023d
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-09-08 16:46:44 +0530

    add carousel

--- client/src/view/LandingPage.jsx
+++ client/src/view/LandingPage.jsx
@@ -1,5 +1,12 @@
 // import React from 'react';
-import homeImg from "../assets/LandingPagee.png";
+import Accessories from "../assets/carousels/Accesaries.png";
+import Cloths from "../assets/carousels/Cloths.png";
+import Electronics from "../assets/carousels/Electronics.png";
+import Ethinic from "../assets/carousels/Ethinic.png";
+import FlowerPots from "../assets/carousels/flrPots.png";
+import Jwellary from "../assets/carousels/Jwellary.png";
+import Gadgets from "../assets/carousels/Gadgets.png";
+
 import "../Styles/LandingPage.css";
 // import CategoriesData from "../data/Categories.json";
 
@@ -7,8 +14,14 @@ function LandingPage(props) {
   // const categories = CategoriesData.find();
   return (
     <>
-    <div>
-    <img src={homeImg} alt="landing-page" className='Clothes_girlImage'></img>
+    <div className="carousel">
+      <img src={FlowerPots} alt="landing-page" className='carousel_Image'></img>
+    <img src={Accessories} alt="landing-page" className='carousel_Image'></img>
+    <img src={Cloths} alt="landing-page" className='carousel_Image'></img>
+    <img src={Electronics} alt="landing-page" className='carousel_Image'></img>
+    <img src={Ethinic} alt="landing-page" className='carousel_Image'></img>
+    <img src={Gadgets} alt="landing-page" className='carousel_Image'></img>
+    <img src={Jwellary} alt="landing-page" className='carousel_Image'></img>
     </div>
 
     <div>
