f58415a0c2242a926d2bca9a92d9863fc15e3aa2
rajnandinitikkal <rajnandinitikkal@gmail.com>
2025-09-09 15:31:19 +0530

    add new carousel at homepage

--- client/src/view/LandingPage.jsx
+++ client/src/view/LandingPage.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import Accessories from "../assets/carousels/Accesaries.png";
+import WatchImg from "../assets/carousels/electric.png";
 import Cloths from "../assets/carousels/Cloths.png";
 import Electronics from "../assets/carousels/Electronics.png";
 import Ethinic from "../assets/carousels/Ethinic.png";
@@ -8,30 +8,43 @@ import Jwellary from "../assets/carousels/Jwellary.png";
 import Gadgets from "../assets/carousels/Gadgets.png";
 
 import "../Styles/LandingPage.css";
-// import CategoriesData from "../data/Categories.json";
+import { useEffect, useState } from "react";
 
-function LandingPage(props) {
-  // const categories = CategoriesData.find();
+function LandingPage() {
+  const images = [
+    FlowerPots,
+    WatchImg,
+    Cloths,
+    Electronics,
+    Ethinic,
+    Gadgets,
+    Jwellary
+  ];
+  const [current, setCurrent] = useState(0);
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setCurrent((prev) => (prev + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [images.length]);
   return (
     <>
-    <div className="carousel">
-      <img src={FlowerPots} alt="landing-page" className='carousel_Image'></img>
-    <img src={Accessories} alt="landing-page" className='carousel_Image'></img>
-    <img src={Cloths} alt="landing-page" className='carousel_Image'></img>
-    <img src={Electronics} alt="landing-page" className='carousel_Image'></img>
-    <img src={Ethinic} alt="landing-page" className='carousel_Image'></img>
-    <img src={Gadgets} alt="landing-page" className='carousel_Image'></img>
-    <img src={Jwellary} alt="landing-page" className='carousel_Image'></img>
+    <div className="slider-container">
+      <div className="slider-track" 
+      style={{ transform: `translateX(-${current * 100}%)` }}>
+        {images.map((img, index) =>(
+          <div className="slide" key={index}>
+          <img
+           src={img} 
+           alt={`carousel - ${index}`} ></img>
+           </div>
+        ))}
+      </div>
     </div>
 
     <div>
-      {/* <h2>Categories</h2>
-      <div>
-        <img src={categories.imgUrl} alt={categories.name}></img>
-        <p>{categories.name}</p>
-      </div> */}
-
-
+       <h2>Shop By Categories</h2>
     </div>
 
     <div>
